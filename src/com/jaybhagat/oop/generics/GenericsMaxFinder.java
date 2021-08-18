9f227eab124c1921c868e62b092b4199900c163b
unknown <RockstarJay>
2021-08-18 21:28:21 +0530

    Added: welcome message

---
+++ src/com/jaybhagat/oop/generics/GenericsMaxFinder.java
@@ -0,0 +1,12 @@
+package com.jaybhagat.oop.generics;
+
+/*
+ * class to print the welcome message on master
+ * @author com.jaybhagat
+ */
+
+public class GenericsMaxFinder {
+	public static void main(String[] args) {
+		System.out.println("Welconme To Java Generics Programmimg.");
+	}
+}
